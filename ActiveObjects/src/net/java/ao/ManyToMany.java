6ebd24c3a879d3efd9a986fb7aa303d9ccad2dac
Daniel Spiewak <djspiewak@gmail.com>
2007-05-16 15:16:34 +0000

    Added ManyToMany annotation
    git-svn-id: https://activeobjects.dev.java.net/svn/activeobjects/trunk@22 69e892e8-b12f-0410-b7e1-b50061bc6545

---
+++ ActiveObjects/src/net/java/ao/ManyToMany.java
@@ -0,0 +1,21 @@
+/*
+ * Created on May 3, 2007
+ */
+package net.java.ao;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Daniel Spiewak
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ManyToMany {
+	String table() default "";
+	
+	String mapSelfField() default "";
+	String mapForeignField() default "";
+}
