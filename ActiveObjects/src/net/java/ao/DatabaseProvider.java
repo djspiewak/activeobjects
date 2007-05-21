a149f0232e7c30fa84026b4fda8bade42a7c5551
Daniel Spiewak <djspiewak@gmail.com>
2007-05-21 00:17:23 +0000

    Implemented type converter method
    git-svn-id: https://activeobjects.dev.java.net/svn/activeobjects/trunk@59 69e892e8-b12f-0410-b7e1-b50061bc6545

--- ActiveObjects/src/net/java/ao/DatabaseProvider.java
+++ ActiveObjects/src/net/java/ao/DatabaseProvider.java
@@ -6,6 +6,7 @@ package net.java.ao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Types;
 
 import net.java.ao.db.IDatabaseProvider;
 import net.java.ao.db.SupportedDBProvider;
@@ -62,6 +63,83 @@ public abstract class DatabaseProvider implements IDatabaseProvider {
 	}
 	
 	protected static String convertTypeToString(int type) {
-		return "";
+		switch (type) {
+			case Types.BIGINT:
+				return "BIGINT";
+				
+			case Types.BINARY:
+				return "BINARY";
+				
+			case Types.BIT:
+				return "BIT";
+				
+			case Types.BLOB:
+				return "BLOB";
+				
+			case Types.BOOLEAN:
+				return "BOOLEAN";
+				
+			case Types.CHAR:
+				return "CHAR";
+				
+			case Types.CLOB:
+				return "CLOB";
+				
+			case Types.DATE:
+				return "DATE";
+				
+			case Types.DECIMAL:
+				return "DECIMAL";
+				
+			case Types.DOUBLE:
+				return "DOUBLE";
+				
+			case Types.FLOAT:
+				return "FLOAT";
+				
+			case Types.INTEGER:
+				return "FLOAT";
+				
+			case Types.LONGVARBINARY:
+				return "LONGVARBINARY";
+			
+			case Types.LONGVARCHAR:
+				return "LONGVARCHAR";
+				
+			case Types.NULL:
+				return "NULL";
+				
+			case Types.NUMERIC:
+				return "NUMERIC";
+				
+			case Types.REAL:
+				return "REAL";
+				
+			case Types.REF:
+				return "REF";
+				
+			case Types.SMALLINT:
+				return "SMALLINT";
+				
+			case Types.SQLXML:
+				return "SQLXML";
+				
+			case Types.STRUCT:
+				return "STRUCT";
+				
+			case Types.TIME:
+				return "TIME";
+				
+			case Types.TIMESTAMP:
+				return "TIMESTAMP";
+				
+			case Types.VARBINARY:
+				return "VARBINARY";
+				
+			case Types.VARCHAR:
+				return "VARCHAR";
+		}
+		
+		return null;
 	}
 }
