d6b2898d60551f84a21aca2627d93091d4eb4c7b
Daniel Spiewak <djspiewak@gmail.com>
2007-05-31 07:31:00 +0000

    Added basic Oracle provider
    git-svn-id: https://activeobjects.dev.java.net/svn/activeobjects/trunk@137 69e892e8-b12f-0410-b7e1-b50061bc6545

--- ActiveObjects/src/net/java/ao/db/OracleDatabaseProvider.java
+++ ActiveObjects/src/net/java/ao/db/OracleDatabaseProvider.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2007, Daniel Spiewak
  * All rights reserved
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -30,62 +30,27 @@
  */
 package net.java.ao.db;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
+import java.sql.Driver;
 
 import net.java.ao.DatabaseProvider;
+import net.java.ao.schema.ddl.DDLTable;
 
 /**
  * @author Daniel Spiewak
  */
-public enum SupportedDBProvider {
-	MYSQL("jdbc:mysql", MySQLDatabaseProvider.class),
-	DERBY("jdbc:derby", EmbeddedDerbyDatabaseProvider.class),
-	ORACLE_THIN("jdbc:oracle:thin", OracleDatabaseProvider.class),
-	ORACLE_OCI("jdbc:oracle:oci", OracleDatabaseProvider.class);
-	
-	private String prefix;
-	private Class<? extends DatabaseProvider> type;
-	
-	private SupportedDBProvider(String prefix, Class<? extends DatabaseProvider> type) {
-		this.prefix = prefix;
-		this.type = type;
+public class OracleDatabaseProvider extends DatabaseProvider {
+
+	public OracleDatabaseProvider(String uri, String username, String password) {
+		super(uri, username, password);
 	}
-	
-	public String getPrefix() {
-		return prefix;
+
+	@Override
+	public Class<? extends Driver> getDriverClass() throws ClassNotFoundException {
+		return (Class<? extends Driver>) Class.forName("oracle.jdbc.OracleDriver");
 	}
-	
-	public Class<? extends DatabaseProvider> getType() {
-		return type;
-	}
-	
-	public DatabaseProvider createInstance(String uri, String username, String password) {
-		DatabaseProvider back = null;
-		
-		try {
-			Constructor<? extends DatabaseProvider> constructor = type.getDeclaredConstructor(String.class, String.class, String.class);
-			constructor.setAccessible(true);
-			
-			back = constructor.newInstance(uri, username, password);
-		} catch (SecurityException e) {
-		} catch (NoSuchMethodException e) {
-		} catch (IllegalArgumentException e) {
-		} catch (InstantiationException e) {
-		} catch (IllegalAccessException e) {
-		} catch (InvocationTargetException e) {
-		}
-		
-		return back;
-	}
-	
-	public static SupportedDBProvider getProviderForURI(String uri) {
-		for (SupportedDBProvider provider : values()) {
-			if (uri.trim().startsWith(provider.prefix.trim())) {
-				return provider;
-			}
-		}
-		
-		return null;
+
+	@Override
+	public String render(DDLTable table) {
+		throw new UnsupportedOperationException("Oracle DDL generation currently not supported");		// TODO	implement
 	}
 }
