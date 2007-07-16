c0b84ea74a340a83b424a977fed59fa1cde8a8e6
Daniel Spiewak <djspiewak@gmail.com>
2007-07-16 23:45:40 +0000

    Removed boolean property from @Index
    git-svn-id: https://activeobjects.dev.java.net/svn/activeobjects/trunk@278 69e892e8-b12f-0410-b7e1-b50061bc6545

--- ActiveObjects/src/net/java/ao/Index.java
+++ ActiveObjects/src/net/java/ao/Index.java
@@ -40,6 +40,4 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface Index {
-	boolean value() default false;
-}
+public @interface Index {}
