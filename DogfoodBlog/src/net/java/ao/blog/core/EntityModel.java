06767596acf6c5b22d6535b1a7a16279bb223276
Eelco Hillenius <eelco.hillenius@gmail.com>
2007-07-23 07:34:31 +0000

    removed @Override annots on interface methods (1.6 only)
    git-svn-id: https://activeobjects.dev.java.net/svn/activeobjects/trunk@314 69e892e8-b12f-0410-b7e1-b50061bc6545

--- DogfoodBlog/src/net/java/ao/blog/core/EntityModel.java
+++ DogfoodBlog/src/net/java/ao/blog/core/EntityModel.java
@@ -28,31 +28,39 @@ import org.apache.wicket.model.IModel;
  * @author Daniel Spiewak
  */
 public abstract class EntityModel implements IModel, Serializable {
+
 	private int id;
-	private Class<? extends Entity> type;
-	
+
 	private String property;
-	
+
+	private Class<? extends Entity> type;
+
 	public EntityModel(Entity entity, String property) {
 		id = entity.getID();
 		type = entity.getEntityType();
-		
+
 		this.property = property;
 	}
-	
+
+	public void detach() {
+	}
+
+	public Entity getEntity() {
+		return getEntityManager().get(type, id);
+	}
+
 	public abstract EntityManager getEntityManager();
 
-	@Override
 	public Object getObject() {
 		String capitolizedProperty = Character.toUpperCase(property.charAt(0)) + property.substring(1);
 		Method m = null;
-		
+
 		try {
 			m = type.getMethod("get" + capitolizedProperty);
 		} catch (SecurityException e) {
 		} catch (NoSuchMethodException e) {
 		}
-		
+
 		if (m == null) {
 			try {
 				m = type.getMethod("is" + capitolizedProperty);
@@ -60,50 +68,41 @@ public abstract class EntityModel implements IModel, Serializable {
 			} catch (NoSuchMethodException e) {
 			}
 		}
-		
+
 		if (m == null) {
 			return null;
 		}
-		
+
 		try {
 			return m.invoke(getEntity());
 		} catch (IllegalArgumentException e) {
 		} catch (IllegalAccessException e) {
 		} catch (InvocationTargetException e) {
 		}
-		
+
 		return null;
 	}
 
-	@Override
 	public void setObject(Object object) {
 		String capitolizedProperty = Character.toUpperCase(property.charAt(0)) + property.substring(1);
 		Method m = null;
-		
+
 		for (Method method : type.getMethods()) {
 			if (method.getName().equals("set" + capitolizedProperty)) {
 				m = method;
 				break;
 			}
 		}
-		
+
 		if (m == null) {
 			return;
 		}
-		
+
 		try {
 			m.invoke(getEntity(), object);
 		} catch (IllegalArgumentException e) {
 		} catch (IllegalAccessException e) {
 		} catch (InvocationTargetException e) {
 		}
-	}
-
-	@Override
-	public void detach() {
-	}
-	
-	public Entity getEntity() {
-		return getEntityManager().get(type, id);
 	}
 }
