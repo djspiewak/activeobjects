2dd4a40a0f2dd97ce64126fd8582425ac7ef83bb
Daniel Spiewak <djspiewak@gmail.com>
2007-08-23 21:00:26 +0000

    Implemented relations cache
    git-svn-id: https://activeobjects.dev.java.net/svn/activeobjects/trunk@414 69e892e8-b12f-0410-b7e1-b50061bc6545

--- ActiveObjects/src/net/java/ao/RelationsCache.java
+++ ActiveObjects/src/net/java/ao/RelationsCache.java
@@ -15,23 +15,114 @@
  */
 package net.java.ao;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * @author Daniel Spiewak
  */
 class RelationsCache {
-	
+	private final Map<CacheKey, Entity[]> cache;
+	private final ReadWriteLock lock;
+
 	public RelationsCache() {
+		cache = new HashMap<CacheKey, Entity[]>();
+		lock = new ReentrantReadWriteLock();
 	}
-	
+
 	public void put(Entity from, Entity[] to) {
-		// TODO
+		if (to.length == 0) {
+			return;
+		}
+		
+		CacheKey key = new CacheKey(from, to[0].getEntityType());
+		lock.writeLock().lock();
+		try {
+			cache.put(key, to);
+		} finally {
+			lock.writeLock().unlock();
+		}
 	}
-	
-	public void get(Entity from, Class<? extends Entity> toType) {
-		// TODO
+
+	public Entity[] get(Entity from, Class<? extends Entity> toType) {
+		lock.readLock().lock();
+		try {
+			return cache.get(new CacheKey(from, toType));
+		} finally {
+			lock.readLock().unlock();
+		}
 	}
-	
+
 	public boolean contains(Entity from, Class<? extends Entity> toType) {
-		return false;		// TODO
+		lock.readLock().lock();
+		try {
+			return cache.containsKey(new CacheKey(from, toType));
+		} finally {
+			lock.readLock().unlock();
+		}
+	}
+
+	private static class CacheKey {
+		private Entity from;
+		private Class<? extends Entity> toType;
+		
+		public CacheKey(Entity from, Class<? extends Entity> toType) {
+			this.from = from;
+			this.toType = toType;
+		}
+
+		public Entity getFrom() {
+			return from;
+		}
+
+		public void setFrom(Entity from) {
+			this.from = from;
+		}
+
+		public Class<? extends Entity> getToType() {
+			return toType;
+		}
+
+		public void setToType(Class<? extends Entity> toType) {
+			this.toType = toType;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (obj instanceof CacheKey) {
+				CacheKey key = (CacheKey) obj;
+				
+				if (key.getFrom() != null) {
+					if (!key.getFrom().equals(from)) {
+						return false;
+					}
+				}
+				if (key.getToType() != null) {
+					if (!key.getToType().equals(toType)) {
+						return false;
+					}
+				}
+				
+				return true;
+			}
+			
+			return super.equals(obj);
+		}
+		
+		@Override
+		public int hashCode() {
+			int hashCode = 0;
+			
+			if (from != null) {
+				hashCode += from.hashCode();
+			}
+			if (toType != null) {
+				hashCode += toType.hashCode();
+			}
+			
+			return hashCode;
+		}
 	}
 }
