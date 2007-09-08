2a77ca6df3c8526fc593bd414839956de3c7ca4f
Daniel Spiewak <djspiewak@gmail.com>
2007-09-08 06:31:35 +0000

    Refactored data into EntityTest superclass
    git-svn-id: https://activeobjects.dev.java.net/svn/activeobjects/trunk@474 69e892e8-b12f-0410-b7e1-b50061bc6545

--- ActiveObjects/test/net/java/ao/EntityTest.java
+++ ActiveObjects/test/net/java/ao/EntityTest.java
@@ -24,196 +24,18 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import test.schema.Company;
 import test.schema.Pen;
 import test.schema.Person;
 import test.schema.PersonLegalDefence;
-import test.schema.PersonSuit;
 
 /**
  * @author Daniel Spiewak
  */
-public class EntityTest {
-	private static EntityManager manager;
-	private static int personID, companyID;
-	
-	private static int[] penIDs;
-	private static int[] defenceIDs;
-	private static int[] suitIDs;
-
-	@BeforeClass
-	public static void setUp() throws SQLException {
-		manager = new EntityManager("jdbc:derby:test_database;create=true", "sa", "jeffbridges");
-		manager.migrate(PersonSuit.class, Pen.class);
-		
-		Logger logger = Logger.getLogger("net.java.ao");
-		Logger l = logger;
-		
-		while ((l = l.getParent()) != null) {
-			for (Handler h : l.getHandlers()) {
-				l.removeHandler(h);
-			}
-		}
-		
-		logger.setLevel(Level.FINE);
-		logger.addHandler(SQLLogMonitor.getInstance());
-		
-		Connection conn = manager.getProvider().getConnection();
-		try {
-			PreparedStatement stmt = conn.prepareStatement("INSERT INTO company (name, cool) VALUES (?,?)", 
-					PreparedStatement.RETURN_GENERATED_KEYS);
-			
-			stmt.setString(1, "Company Name");
-			stmt.setBoolean(2, false);
-			
-			stmt.executeUpdate();
-			
-			ResultSet res = stmt.getGeneratedKeys();
-			if (res.next()) {
-				companyID = res.getInt(1);
-			}
-			res.close();
-			stmt.close();
-			
-			stmt = conn.prepareStatement("INSERT INTO person (firstName, companyID) VALUES (?, ?)", 
-					PreparedStatement.RETURN_GENERATED_KEYS);
-			
-			stmt.setString(1, "Daniel");
-			stmt.setInt(2, companyID);
-			
-			stmt.executeUpdate();
-			
-			res = stmt.getGeneratedKeys();
-			if (res.next()) {
-				personID = res.getInt(1);
-			}
-			res.close();
-			stmt.close();
-			
-			penIDs = new int[3];
-			stmt = conn.prepareStatement("INSERT INTO pen (width,personID) VALUES (?,?)", 
-					PreparedStatement.RETURN_GENERATED_KEYS);
-
-			int index = 0;
-			
-			stmt.setDouble(1, 0.5);
-			stmt.setInt(2, personID);
-			stmt.executeUpdate();
-			
-			res = stmt.getGeneratedKeys();
-			if (res.next()) {
-				penIDs[index++] = res.getInt(1);
-			}
-			res.close();
-			
-			stmt.setDouble(1, 0.7);
-			stmt.setInt(2, personID);
-			stmt.executeUpdate();
-			
-			res = stmt.getGeneratedKeys();
-			if (res.next()) {
-				penIDs[index++] = res.getInt(1);
-			}
-			res.close();
-			
-			stmt.setDouble(1, 1);
-			stmt.setInt(2, personID);
-			stmt.executeUpdate();
-			
-			res = stmt.getGeneratedKeys();
-			if (res.next()) {
-				penIDs[index++] = res.getInt(1);
-			}
-			res.close();
-			
-			stmt.close();
-			
-			defenceIDs = new int[3];
-			stmt = conn.prepareStatement("INSERT INTO personDefence (severity) VALUES (?)", 
-					PreparedStatement.RETURN_GENERATED_KEYS);
-			
-			index = 0;
-
-			stmt.setInt(1, 5);
-			stmt.executeUpdate();
-
-			res = stmt.getGeneratedKeys();
-			if (res.next()) {
-				defenceIDs[index++] = res.getInt(1);
-			}
-			res.close();
-			
-			stmt.setInt(1, 7);
-			stmt.executeUpdate();
-
-			res = stmt.getGeneratedKeys();
-			if (res.next()) {
-				defenceIDs[index++] = res.getInt(1);
-			}
-			res.close();
-			
-			stmt.setInt(1, 1);
-			stmt.executeUpdate();
-
-			res = stmt.getGeneratedKeys();
-			if (res.next()) {
-				defenceIDs[index++] = res.getInt(1);
-			}
-			res.close();
-			
-			stmt.close();
-			
-			suitIDs = new int[3];
-			stmt = conn.prepareStatement("INSERT INTO personSuit (personID, personLegalDefenceID) VALUES (?,?)",
-					PreparedStatement.RETURN_GENERATED_KEYS);
-
-			index = 0;
-			
-			stmt.setInt(1, personID);
-			stmt.setInt(2, defenceIDs[0]);
-			stmt.executeUpdate();
-			
-			res = stmt.getGeneratedKeys();
-			if (res.next()) {
-				suitIDs[index++] = res.getInt(1);
-			}
-			res.close();
-			
-			stmt.setInt(1, personID);
-			stmt.setInt(2, defenceIDs[1]);
-			stmt.executeUpdate();
-			
-			res = stmt.getGeneratedKeys();
-			if (res.next()) {
-				suitIDs[index++] = res.getInt(1);
-			}
-			res.close();
-			
-			stmt.setInt(1, personID);
-			stmt.setInt(2, defenceIDs[2]);
-			stmt.executeUpdate();
-			
-			res = stmt.getGeneratedKeys();
-			if (res.next()) {
-				suitIDs[index++] = res.getInt(1);
-			}
-			res.close();
-			
-			stmt.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			conn.close();
-		}
-	}
+public class EntityTest extends DataTest {
 	
 	@Test
 	public void testDatabaseAccessor() {
@@ -440,23 +262,5 @@ public class EntityTest {
 		SQLLogMonitor.getInstance().markWatchSQL();
 		person.getPersonLegalDefences();
 		assertFalse(SQLLogMonitor.getInstance().isExecutedSQL());
-	}
-	
-	@AfterClass
-	public static void tearDown() throws SQLException {
-		for (int id : penIDs) {
-			manager.delete(manager.get(Pen.class, id));
-		}
-		for (int id : suitIDs) {
-			manager.delete(manager.get(PersonSuit.class, id));
-		}
-		for (int id : defenceIDs) {
-			manager.delete(manager.get(PersonLegalDefence.class, id));
-		}
-		
-		manager.delete(manager.get(Person.class, personID));
-		manager.delete(manager.get(Company.class, companyID));
-		
-		manager.getProvider().dispose();
 	}
 }
