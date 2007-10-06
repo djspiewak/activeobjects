32cdb9f7d4fdf13b37b17f48e145004075fdf1b1
Daniel Spiewak <djspiewak@gmail.com>
2007-10-06 19:39:27 +0000

    Javadoc
    git-svn-id: https://activeobjects.dev.java.net/svn/activeobjects/trunk@580 69e892e8-b12f-0410-b7e1-b50061bc6545

--- ActiveObjects/src/net/java/ao/Generator.java
+++ ActiveObjects/src/net/java/ao/Generator.java
@@ -21,10 +21,32 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * <p>Marks a specific method (and thus, database field) to contain
+ * a generated value on INSERT.  The precise value and generation
+ * algorithm is specified by the {@link ValueGenerator} class.
+ * This can be used to implement functionality like UUID keys
+ * and other values which must be generated in the application
+ * layer.</p>
+ * 
+ * <p>This only generates values for INSERT statements, not
+ * UPDATE, SELECT or any such statement.  It is likely that down
+ * the road, such functionality will be added.  However, at the
+ * moment the designed use-case is something like UUID
+ * primary keys, rather than an application-layer ON UPDATE.</p>
+ * 
  * @author Daniel Spiewak
+ * @see net.java.ao.schema.OnUpdate
+ * @see net.java.ao.ValueGenerator
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Generator {
+	
+	/**
+	 * Specifies the generator class to use in creating new values
+	 * for the field in question.  This generator should be
+	 * thread-safe and execute very quickly.
+	 *
+	 */
 	Class<? extends ValueGenerator<?>> value();
 }
