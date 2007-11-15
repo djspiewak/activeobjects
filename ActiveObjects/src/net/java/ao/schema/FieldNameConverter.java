c2d314cf1457d190ad2da10fdcd0730cb5b8d61f
Daniel Spiewak <djspiewak@gmail.com>
2007-11-15 04:52:25 +0000

    Some javadoc
    git-svn-id: https://activeobjects.dev.java.net/svn/activeobjects/trunk@672 69e892e8-b12f-0410-b7e1-b50061bc6545

--- ActiveObjects/src/net/java/ao/schema/FieldNameConverter.java
+++ ActiveObjects/src/net/java/ao/schema/FieldNameConverter.java
@@ -17,9 +17,25 @@ package net.java.ao.schema;
 
 import java.lang.reflect.Method;
 
+import net.java.ao.EntityManager;
 import net.java.ao.RawEntity;
 
 /**
+ * <p>Superinterface to all field name converters; designed to impose conventions
+ * upon the auto-conversion of method names to database fields.  The idea
+ * behind this is to allow user-specified field name conventions and standards
+ * rather than always enforcing ActiveObjects's idea of "good naming".</p>
+ * 
+ * <p>Every {@link EntityManager} contains a single field name converter which
+ * the entire library uses when performing operations.  Any third-party code which
+ * interacts with database fields can also make use of this class.  However, it's
+ * significantly harder to do so because Java doesn't support method literals.</p>
+ * 
+ * <p>Most new implementations of field name converters should extend 
+ * {@link AbstractFieldNameConverter} rather than implementing this interface
+ * directly.  This allows third-party converters to take advantage of boiler-plate
+ * conversion code which would otherwise have to be duplicated in every converter.</p>
+ * 
  * @author Daniel Spiewak
  */
 public interface FieldNameConverter {
