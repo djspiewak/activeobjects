fd56bc3cb2f16f8e75283465be5eb6788fd814dd
Daniel Spiewak <djspiewak@gmail.com>
2007-11-29 19:24:57 +0000

    Javadoc
    git-svn-id: https://activeobjects.dev.java.net/svn/activeobjects/trunk@693 69e892e8-b12f-0410-b7e1-b50061bc6545

--- ActiveObjects/src/net/java/ao/Polymorphic.java
+++ ActiveObjects/src/net/java/ao/Polymorphic.java
@@ -21,6 +21,36 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * <p>Tags a given entity type as polymorphically abstract.  This means that
+ * the given type will <i>not</i> peer to any table, but rather represent
+ * a polymorphic supertype to other entities (entities which extend the
+ * given interface).  Unlike conventional inheritence, which also causes
+ * the supertype to not peer to a table, polymorphic type inheritence
+ * allows instances of the subtype to be stored in the database into fields
+ * which are "typed" in ActiveObjects as the supertype.  All of this is
+ * really much simpler than it sounds:</p>
+ * 
+ * <pre>public interface Person extends Entity {
+ *     public Computer getComputer();
+ *     public void setComputer(Computer computer);
+ * }
+ * 
+ * &#064;Polymorphic
+ * public interface Computer extends Entity {
+ *     public float getSpeed();
+ *     public void setSpeed(float speed);
+ * }
+ * 
+ * public interface Mac extends Computer {}
+ * public interface PC extends Computer {}</pre>
+ * 
+ * <p>In this case, <code>Computer</code> does not correspond with any table in
+ * the database.  Likewise, <code>Person</code> has no foreign keys for the
+ * corresponding field to <code>getComputer()</code>.  When an entity type is
+ * polymorphic, its subtypes can be used polymorphically as they are in
+ * <code>Person</code>.  This is essentially the conceptual analog of an
+ * abstract class when mapped into the database.</p>
+ * 
  * @author Daniel Spiewak
  */
 @Retention(RetentionPolicy.RUNTIME)
