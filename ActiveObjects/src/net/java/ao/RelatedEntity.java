31bb4e2da32f22cf98a7980e0b173a64354ef9f5
Daniel Spiewak <djspiewak@gmail.com>
2007-11-29 19:27:00 +0000

    Fixed annotation escape code
    git-svn-id: https://activeobjects.dev.java.net/svn/activeobjects/trunk@697 69e892e8-b12f-0410-b7e1-b50061bc6545

--- ActiveObjects/src/net/java/ao/RelatedEntity.java
+++ ActiveObjects/src/net/java/ao/RelatedEntity.java
@@ -30,12 +30,12 @@ import net.java.ao.schema.Ignore;
  * <pre>public interface Post extends Entity, RelatedEntity&lt;Post&gt; {
  *     // ...
  *     
- *     @Searchable
- *     @SQLType(Types.BLOB)
+ *     &#064;Searchable
+ *     &#064;SQLType(Types.BLOB)
  *     public String getBody();
  *     
- *     @Searchable
- *     @SQLType(Types.BLOB)
+ *     &#064;Searchable
+ *     &#064;SQLType(Types.BLOB)
  *     public void setBody(String body);
  * }
  * 
