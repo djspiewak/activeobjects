5ee4523691d974b3522612f059c2f4148d40f312
Daniel Spiewak <djspiewak@gmail.com>
2007-11-29 19:28:53 +0000

    Fixed annotation escape code
    git-svn-id: https://activeobjects.dev.java.net/svn/activeobjects/trunk@702 69e892e8-b12f-0410-b7e1-b50061bc6545

--- ActiveObjects/src/net/java/ao/SearchableEntityManager.java
+++ ActiveObjects/src/net/java/ao/SearchableEntityManager.java
@@ -48,12 +48,12 @@ import org.apache.lucene.store.Directory;
  * <pre>public interface Post extends Entity {
  *     // ...
  *     
- *     @Searchable
- *     @SQLType(Types.BLOB)
+ *     &#064;Searchable
+ *     &#064;SQLType(Types.BLOB)
  *     public String getBody();
  *     
- *     @Searchable
- *     @SQLType(Types.BLOB)
+ *     &#064;Searchable
+ *     &#064;SQLType(Types.BLOB)
  *     public void setBody(String body);
  * }
  * 
