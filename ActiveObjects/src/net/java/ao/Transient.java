5e1015613d23abf663d326ce4dca2fbca5cb4a9a
Daniel Spiewak <djspiewak@gmail.com>
2008-01-29 22:49:58 +0000

    Added javadoc
    git-svn-id: https://activeobjects.dev.java.net/svn/activeobjects/trunk@811 69e892e8-b12f-0410-b7e1-b50061bc6545

--- ActiveObjects/src/net/java/ao/Transient.java
+++ ActiveObjects/src/net/java/ao/Transient.java
@@ -21,6 +21,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Marks a method such that its field should not be stored within the
+ * value cache.  This allows for scenarios when the you know that the value
+ * of the field will be updated in an external process or perhaps the value
+ * will be too large to be reasonably cached.
+ * 
  * @author Daniel Spiewak
  */
 @Retention(RetentionPolicy.RUNTIME)
